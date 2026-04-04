76505484adcbb1707edd7d3f964623c03e831af8
YURI-gomes062 <yuri.lu312@gmail.com>
2026-04-04 20:13:12 -0300

    04/04/206

---
+++ exerc6.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+
+void approved (float nota){
+    if(nota >= 6){
+        printf("Aprovado\n");
+    }else{
+        printf("Reprovado\n");
+    }
+}
+
+float media(float n1,float n2,float n3){
+
+    return (n1 + n2 + n3 ) / 3;
+}
+
+int main(){
+
+    float no1,no2,no3,media1;
+    scanf("%f %f %f", &no1, &no2, &no3);
+
+    media1 = media(no1, no2, no3);
+    approved(media1);
+
+
+    return 0;
+}
