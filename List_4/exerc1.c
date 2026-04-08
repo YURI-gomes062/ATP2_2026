4bd2c0ba50ef6f9e4ce8b983194a1d6f21b6f162
YURI-gomes062 <yuri.lu312@gmail.com>
2026-04-08 08:13:05 -0300

    08/04/2026

---
+++ List_4/exerc1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int banana = 10;
+
+int main(){
+
+    int banana = 15;
+    printf("%i\n", banana);
+    variavel();
+}
+int variavel(){
+
+    printf("%i\n", banana);
+
+    return 0;
+}
---
+++ List_4/exerc2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    int num;
+    scanf("%i", &num);
+    printf("%i", num);
+
+    alter(num);
+    return 0;
+}
+int alter(int ){
+
+    
+
+    return 0;
+}
