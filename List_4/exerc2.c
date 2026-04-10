70399184e0569e2ad2c2a2aef2c12ce4e5f1b0f1
YURI-gomes062 <yuri.lu312@gmail.com>
2026-04-10 18:50:27 -0300

    10/04/2026

--- List_4/exerc2.c
+++ List_4/exerc2.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
+void t(int b);
 
 int main(){
 
-    int num;
-    scanf("%i", &num);
-    printf("%i", num);
-
-    alter(num);
+    int a;
+    scanf("%d", &a);
+    printf("%d\n", a);
+    t(a);
+    
     return 0;
 }
-int alter(int ){
-
-    
-
-    return 0;
+void t(int b){
+    b = b * 2;
+    printf("%d",b);
 }
