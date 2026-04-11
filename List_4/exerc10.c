bef576e9e5adbf9acdf276961cffbb77e2706a4d
YURI-gomes062 <yuri.lu312@gmail.com>
2026-04-11 13:24:38 -0300

    11/04/2026

--- List_4/exerc10.c
+++ List_4/exerc10.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
+int value2(int *value11);
+int value1(int *value22);
 
-int value1(int *value11){ // A primeira função é responsável por adicionar +10 ao valor da variável.
-
-    return *value11 + 10;
-}
-int value2(int *value22){ // a segunda função consiste em adicionar +20 ao valor da variável. 
-
-    return *value22 + 20; 
-}
 void main(){
 
     int value;
@@ -22,4 +16,12 @@ void main(){
     printf("-- terceiro valor, alterado por passagem de refêrencia pela função value2. [ %d ]\n", value); // -- terceiro valor, alterado por passagem de refêrencia pela função value2.
 
 
+}
+int value1(int *value11){ // A primeira função é responsável por adicionar +10 ao valor da variável.
+
+    return *value11 = *value11 + 10;
+}
+int value2(int *value22){ // a segunda função consiste em adicionar +20 ao valor da variável. 
+
+    return *value22 = *value22 + 20; 
 }
