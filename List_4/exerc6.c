2ae00e804228e3d0d37533f15704e6af5050d278
YURI-gomes062 <yuri.lu312@gmail.com>
2026-04-11 09:24:34 -0300

    11/04/2026

---
+++ List_4/exerc6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int triple(int num1);
+
+int main(){
+
+    int num;
+    scanf("%d", &num);
+    printf("%d\n", triple(num));
+
+    return 0;
+}
+int triple(int num1){
+
+    return num1 = num1 * 3;;
+}
