9222a2a950074027a462351a9f4ee64acbe4a2bb
YURI-gomes062 <yuri.lu312@gmail.com>
2026-04-11 12:17:32 -0300

    11/04/2026

---
+++ List_4/exerc10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int value1(int *value11){ // A primeira função é responsável por adicionar +10 ao valor da variável.
+
+    return *value11 + 10;
+}
+int value2(int *value22){ // a segunda função consiste em adicionar +20 ao valor da variável. 
+
+    return *value22 + 20; 
+}
+void main(){
+
+    int value;
+    printf("Algoritmo de alternancia de valores de variável por meio de funções com passagem de refêrencia:\n");
+    scanf("%d", &value);
+    printf("-- Primeiro valor lido, definido pelo user. [ %d ]\n", value); // -- Primeiro valor lido, definido pelo user.
+
+    value = value1(&value);
+    printf("-- Segundo valor, alterado por passagem de refêrencia pela função value1. [ %d ]\n", value); // -- Segundo valor, alterado por passagem de refêrencia pela função value1.
+
+    value = value2(&value);
+    printf("-- terceiro valor, alterado por passagem de refêrencia pela função value2. [ %d ]\n", value); // -- terceiro valor, alterado por passagem de refêrencia pela função value2.
+
+
+}
