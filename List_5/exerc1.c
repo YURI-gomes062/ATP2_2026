2c8153e5d409440b4f625587af9daf832b81a768
YURI-gomes062 <yuri.lu312@gmail.com>
2026-04-14 12:09:12 -0300

    14/04/2026

--- List_5/exerc1.c
+++ List_5/exerc1.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
+
+int fatorial(int nu){
+
+    if(nu == 1 || nu == 0){
+        return 1;
+    }
+
+    return nu * fatorial(nu - 1);
+}
 int main(){
 
-
-    
+    int num;
+    scanf("%d",&num);
+    fatorial(num);
+    printf("%d\n",fatorial(num));
+    return 0;
 }
