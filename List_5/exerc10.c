70f1f9a10f0477ba5dff2e8bf6185f39399cf8b2
YURI_c137 <yuri.lu312@gmail.com>
2026-04-22 11:58:48 -0300

    22/04/2026

--- List_5/exerc10.c
+++ List_5/exerc10.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
+
 int alter_value(int *point, int size);
 
-int main(){
-
-    int nums[5] = {1, 2, 3, 4, 5}; // vetor
+int main() {
+    int nums[5] = {1, 2, 3, 4, 5};
     int *p = nums;
-    alter_value(p,4);
-    printf("%d", *p);
+
+    alter_value(p, 5);
+
+    printf("\nPrimeiro elemento apos a funcao: %d\n", nums[0]);
 
     return 0;
 }
-int alter_value(int *point, int size){
 
-    for(int i = 0; i < size; i++){
-    
-        printf("%d\n", *point);
-        *(point + i) = *(point + i + 1);
+int alter_value(int *point, int size) {
+    for (int i = 0; i < size; i++) {
+        
+        *(point + i) = *(point + i) * 10; // point[i] = point[i] * 10;
+        printf("Processando indice %d: valor alterado para %d\n", i, *(point + i));
     }
     return 0;
-}
+}    
