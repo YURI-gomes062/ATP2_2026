c9ea33b4efc1799e5a2e08420cd29e9a5f4ff554
YURI-gomes062 <yurioficial892@gmail.com>
2026-06-17 16:23:19 -0300

    17/06/2026

---
+++ List_6/exerc6.c
@@ -0,0 +1,17 @@
+#include <stdioh>
+
+int main(){
+
+    int g[10] = {1,2,3,4,5,6,7,8,9,10};
+
+    printf("%d", pairs_cont(10,g));
+
+    return 0;
+}
+int pairs_cont(int t, int g[]){
+
+    
+
+    return 0;
+
+}
